11605bf8a0396e0ca2568bff9f9c8a2701f14c4e
Oness <auditoree9@gmail.com>
2026-09-10 07:25:14 +0000

    added api-gateway and setup product-service routes

---
+++ services/api-gateway/src/main/java/dev/shopsphere/gateway/config/GatewayConfig.java
@@ -0,0 +1,23 @@
+package dev.shopsphere.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Value("${app.routes.product-service-uri:lb://PRODUCT-SERVICE}")
+    private String productServiceUri;
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("product-service", r -> r
+                        .path("/api/v1/products", "/api/v1/products/**")
+                        .uri(productServiceUri))
+                .build();
+    }
+}
